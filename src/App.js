8f690986e1ef8a69023b7ac4c7edbfd980f5456a
Henrique <123227224+RafaelHenriqu@users.noreply.github.com>
2023-09-06 19:07:02 -0300

    MyPortfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
+import Configs from './Components/Configs';
+import Css from './Imagens/Css.png';
+import Gmail from './Imagens/Gmail.png';
+import HTML from './Imagens/Html.png';
+import js from './Imagens/Js.png';
+import Node from './Imagens/Node.png';
+import React from './Imagens/React.png';
+import Scss from './Imagens/Sass.png';
+let Hora = new Date().getHours()
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <>
+    <Configs Hora={Hora} ></Configs>
+    <p className='EsterEgg'>EsterEgg</p>
+    
+   <section id='Sobre-Min'>
+    <div id='Perfil'></div>
+    <h2>Sobre-Mim</h2>
+   <p>Seja bem-vindo(a)! Meu nome é Rafael, tenho {new Date().getFullYear() - 2005} anos e gostaria de poder ajudá-lo(a)
+    com seus projetos. Utilizando minhas habilidades e conhecimentos,
+     posso oferecer soluções eficientes e criativas.
+    Estou à disposição para discutir suas necessidades e contribuir para o sucesso de seus projetos.
+    </p>
+    <h2>Habilidades</h2>
+    <div id='Habilidades'>
+      
+    <img src={HTML} title='HTML'></img>
+    <img src={js} title='JavaScript'></img>
+    <img src={React} title='React'></img>
+    <img src={Css} title='Css'></img>
+    <img src={Scss} title='Scss'></img>
+    <img src={Node} title='Node.js'></img>
+    
     </div>
+
+    <h2>Projetos</h2>
+ 
+    <h2  >Contate-me</h2>
+    <div id='Contate-me'>
+    <a href="mailto:girafaeoficial@gmail.com?subject=Desenvolvedor Web&body=Olá Gostaria de le contratar"><img src={Gmail}></img></a>
+    </div>
+   </section>
+ 
+ </>
+
+
   );
 }
 
---
+++ src/Components/Configs.js
@@ -0,0 +1,20 @@
+/*
+   
+    let Perfil = document.getElementById('Perfil').style
+
+    Perfil.boxShadow = `0px 0px 50px  hsla(240, 100%, 50%, 0.${String(Hora).substring(1)})`
+   
+    if (Hora > 19 || Hora < 11)
+    {
+     Perfil.boxShadow = `0px 0px 50px  hsla(240, 100%, 50%, 0.9)`
+     
+    }
+     
+*/
+function Configs({Hora})
+{
+ 
+
+}
+
+export default Configs 
